8e4e8f5fa053d65b47328978295abde469d782ce
Pedro <pedroklajmands@gmail.com>
2025-11-21 02:01:45 -0300

    Trabalho final 2 questão

--- code/_master.R
+++ code/_master.R
@@ -1,19 +1,18 @@
-# =====================================================
-# MASTER FILE — Caminhos e configurações do projeto
-# Trabalho de Econometria I — IE/UFRJ
-# Grupo: (nomes)
-# Data: Novembro de 2025
-# =====================================================
+# Master file - Configuracoes do projeto
+# Trabalho Econometria I - IE/UFRJ
+#Juliana, Crisly e Tobias
+#Novembro 2025
 
-root <- "C:/Users/Pedro/Documents/Faculdade/econ_2025_grupo_X"  # ALTERAR SE NECESSÁRIO
+# Configuração de caminhos
+path_input  <- "input"
+path_output <- "output"
+path_tmp    <- "tmp"
+path_code   <- "code"
+path_misc   <- "misc"
 
-path_code   <- file.path(root, "code")
-path_input  <- file.path(root, "input")
-path_tmp    <- file.path(root, "tmp")
-path_output <- file.path(root, "output")
-path_misc   <- file.path(root, "misc")
-
-setwd(root)
-
-# Para facilitar os outros scripts:
-source(file.path(path_code, ".funcoes_utilitarias.R"), local = TRUE)
+# Criar pastas
+dir.create(path_input, showWarnings = FALSE)
+dir.create(path_output, showWarnings = FALSE)
+dir.create(path_tmp, showWarnings = FALSE)
+dir.create(path_code, showWarnings = FALSE)
+dir.create(path_misc, showWarnings = FALSE)
